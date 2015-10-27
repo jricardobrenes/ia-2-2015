7a9767a20a645cf5058fe9cdf7f0b579c3659fd0
Sebastián Segura <bastianseguses@gmail.com>
2015-10-27 17:06:10 -0600

    Añadido fire para la pc

--- fire.pl
+++ fire.pl
@@ -10,11 +10,19 @@
 %   - Devuelve la nueva fila con -1 o 1 en la pos. respectiva     %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-shoot(_,_,[],[]).
-shoot(C,C,['X'|T],[-1|T2]):- Cc is C+1, shoot(C,Cc,T,T2), writeln('MISS!!').
-shoot(C,C,[_|T],[1|T2]):- Cc is C+1, shoot(C,Cc,T,T2), writeln('HIT!!').
-shoot(C,Cc,[H|T],[H|T2]):- Cc2 is Cc+1, shoot(C,Cc2,T,T2).
-shoot(C,L,M):-	shoot(C,0,L,M).
+
+plShoot(_,_,[],[]).
+plShoot(C,C,['X'|T],[-1|T2]):- Cc is C+1, plShoot(C,Cc,T,T2), writeln('MISS!!').
+plShoot(C,C,[_|T],[1|T2]):- Cc is C+1, plShoot(C,Cc,T,T2), writeln('HIT!!').
+plShoot(C,Cc,[H|T],[H|T2]):- Cc2 is Cc+1, plShoot(C,Cc2,T,T2).
+plShoot(C,L,M):- plShoot(C,0,L,M).
+
+pcShoot(_,_,_,[],[]).
+pcShoot(C,C,A,[_|T],[A|T2]):- Cc is C+1, pcShoot(C,Cc,A,T,T2).
+pcShoot(C,Cc,A,[H|T],[H|T2]):- Cc2 is Cc+1, pcShoot(C,Cc2,A,T,T2).
+pcShoot(C,A,L,M):- pcShoot(C,0,A,L,M).
+
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % GETROW:			                                  %
@@ -42,8 +50,18 @@ getRow(R,Rc,[H|T],Row,[H|Tl],D):- Rc2 is Rc+1, getRow(R,Rc2,T,Row,Tl,D).
 %     para devolver la nueva matriz                               %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-fire(R,C,B,Result):-
+plFire(R,C,B,Result):-
 	getRow(R,0,B,Row,L,D),
-	shoot(C,Row,NewRow),
+	plShoot(C,Row,NewRow),
 	append(L,[NewRow],M),
 	append(M,D,Result).
+
+%pcFire(B,Result):-
+	%calculate(row,col),
+	%askUser miss/hit,
+	%miss? Action = -1 | hit? Action = 1,
+	%getRow(R,0,B,Row,L,D),
+        %pcShoot(C,Action,Row,NewRow),
+        %append(L,[NewRow],M),
+	%append(M,D,Result).
+
